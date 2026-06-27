eaeb92fa03a14b853f674669335c9c20ba819cc7
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2026-06-27 17:30:59 +0700

    feat: add global 404 endpoint not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './modules/auth/auth.routes';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { MasterRouter } from './modules/backoffice/master/master.router';
 import { errorHandler } from './common/error.middleware';
+import { ApiError } from './common/ApiError';
 
 const app = express();
 
@@ -19,6 +20,10 @@ app.use('/api/backoffice', MasterRouter());
 // Health check
 app.get('/health', (_req: Request, res: Response) => {
   res.status(200).json({ status: 'OK', timestamp: new Date() });
+});
+
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  next(new ApiError('Endpoint not found', 404));
 });
 
 // Error handling middleware
